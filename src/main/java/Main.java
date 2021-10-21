a62ede849009975b63fbb693466228177ef652e8
larutyunov <larutyunov@diasoft.ru>
2021-10-21 17:48:17 +0300

    2 fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import service.QuestionServiceImpl;
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
-        QuestionService service = context.getBean(QuestionServiceImpl.class);
+        QuestionService service = context.getBean(QuestionService.class);
         QuestionService.printResult(service.quizStart());
     }
 }
