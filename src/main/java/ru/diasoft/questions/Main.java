cb362664d03359fee19380cf32ccd822b224c8a9
larutyunov <larutyunov@diasoft.ru>
2021-11-30 23:57:15 +0300

    7 complete hw_4

--- src/main/java/ru/diasoft/questions/Main.java
+++ src/main/java/ru/diasoft/questions/Main.java
@@ -1,29 +1,17 @@
 package ru.diasoft.questions;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.*;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.io.ClassPathResource;
 import ru.diasoft.questions.service.QuestionService;
 
-@Configuration
-@ComponentScan
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-
-        ApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+        ApplicationContext context = SpringApplication.run(Main.class);
         QuestionService service = context.getBean(QuestionService.class);
         service.printResult(service.quizStart());
     }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
-            final var configurer = new PropertySourcesPlaceholderConfigurer();
-            configurer.setLocation(new ClassPathResource("application.properties"));
-            return configurer;
-    }
-
 
 }
